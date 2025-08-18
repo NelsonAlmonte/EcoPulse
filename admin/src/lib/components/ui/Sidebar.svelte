bd57b0bd9aa5fde971809ec476878607996927a6
Nelson Almonte <almontetejedanelson@gmail.com>
2025-08-18 14:31:37 -0400

    feat(admin): add flowbite svelte
    add lucide icons

---
+++ admin/src/lib/components/ui/Sidebar.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import {
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarDropdownWrapper,
+		SidebarButton,
+		uiHelpers,
+		SidebarBrand
+	} from 'flowbite-svelte';
+	import { ChartArea, FileSearch, House, Settings, Users } from '@lucide/svelte';
+	import { page } from '$app/state';
+
+	let activeUrl = $state(page.url.pathname);
+	const spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	const demoSidebarUi = uiHelpers();
+	let isDemoOpen = $state(false);
+	const closeDemoSidebar = demoSidebarUi.close;
+	$effect(() => {
+		isDemoOpen = demoSidebarUi.isOpen;
+		activeUrl = page.url.pathname;
+	});
+</script>
+
+<SidebarButton onclick={demoSidebarUi.toggle} class="mb-2" />
+<div>
+	<Sidebar
+		{activeUrl}
+		backdrop={false}
+		isOpen={isDemoOpen}
+		closeSidebar={closeDemoSidebar}
+		params={{ x: -50, duration: 50 }}
+		position="absolute"
+		classes={{ nonactive: 'p-2', active: 'p-2' }}
+		class="z-50 h-full"
+	>
+		<SidebarGroup>
+			<SidebarBrand>
+				<img src="/img/logo.png" alt="Flowbite Svelte" class="h-6 w-6 rounded-full" />
+				<span class="ml-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+					>EcoPulse</span
+				>
+			</SidebarBrand>
+			<SidebarItem label="Inicio">
+				{#snippet icon()}
+					<House
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+			</SidebarItem>
+			<SidebarDropdownWrapper label="Incidencias" classes={{ btn: 'p-2' }}>
+				{#snippet icon()}
+					<FileSearch
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+				<SidebarItem label="Products" href="" />
+			</SidebarDropdownWrapper>
+			<SidebarDropdownWrapper label="Usuarios" classes={{ btn: 'p-2' }}>
+				{#snippet icon()}
+					<Users
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+				<SidebarItem label="Analítica" href="/" />
+			</SidebarDropdownWrapper>
+			<SidebarItem label="Analítica" {spanClass} href="/components/sidebar">
+				{#snippet icon()}
+					<ChartArea
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+			</SidebarItem>
+		</SidebarGroup>
+		<SidebarGroup border>
+			<SidebarDropdownWrapper label="Setting" classes={{ btn: 'p-2' }}>
+				{#snippet icon()}
+					<Settings
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+				<SidebarItem label="Account" href="" />
+			</SidebarDropdownWrapper>
+		</SidebarGroup>
+	</Sidebar>
+</div>
---
+++ admin/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Sidebar from '$lib/components/ui/Sidebar.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>EcoPulse - Admin</title>
+</svelte:head>
+
+<Sidebar />
+
+<div class="h-96 overflow-auto p-4 md:ms-64">
+	{@render children?.()}
+</div>
