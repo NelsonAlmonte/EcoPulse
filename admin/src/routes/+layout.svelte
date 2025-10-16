aa372f722c55a05c8b16e4bbf553c1a48173bee5
Nelson Almonte <almontetejedanelson@gmail.com>
2025-10-16 15:02:05 -0400

    wip: wip auth

--- admin/src/routes/+layout.svelte
+++ admin/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
 	import favicon from '$lib/assets/favicon.svg';
 	import Sidebar from '$lib/components/ui/Sidebar.svelte';
 	import PageLoad from '$lib/components/ui/PageLoad.svelte';
@@ -7,7 +9,19 @@
 	import Header from '$lib/components/ui/Header.svelte';
 	import Toast from '$lib/components/ui/Toast.svelte';
 
-	let { children } = $props();
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				console.log('newsession', newSession);
+				console.log('sessuib', session);
+				invalidate('supabase:auth');
+			}
+		});
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <svelte:head>
---
+++ admin/src/routes/admin/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	let { data, children } = $props();
+	let { supabase } = $derived(data);
+
+	async function logout() {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error(error);
+		}
+	}
+</script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+	<button onclick={logout}>Logout</button>
+</header>
+<main>
+	{@render children()}
+</main>
